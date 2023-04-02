282005604fe25c7f2e54637f06a9675c918f8820
Valtteri Huuskonen <valtteri.huuskonen@tuni.fi>
2023-04-03 00:00:03 +0300

    Add SQL init script
    Create tables for users, listings, images

---
+++ db/init-scripts/dev/init.sql
@@ -0,0 +1,27 @@
+CREATE TABLE IF NOT EXISTS `users` (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    -- 320 is the max length of an email address https://www.rfc-editor.org/rfc/rfc3696#:~:text=the%20%22%40%22)%20for%20a-,total%20length%20of%20320,-characters.%20%20Systems%20that
+    email VARCHAR(320) NOT NULL,
+    -- bcrypt hashes are 60 characters long
+    password_hash VARCHAR(60) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT ON UPDATE CURRENT_TIMESTAMP
+);
+CREATE TABLE listings (
+    listing_id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
+    asking_price DECIMAL(10, 2) NOT NULL,
+    owner INT NOT NULL,
+    location VARCHAR(255) NOT NULL,
+    picture_url VARCHAR(255) NOT NULL,
+    FOREIGN KEY (owner) REFERENCES users(userid)
+);
+CREATE TABLE IF NOT EXISTS `pictures` (
+    id SERIAL PRIMARY KEY,
+    url VARCHAR(1000) NOT NULL,
+    blurhash VARCHAR(30) NOT NULL,
+    listing_id INT NOT NULL,
+    FOREIGN KEY (listing_id) REFERENCES listings(listingid)
+);
