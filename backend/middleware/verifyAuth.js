7adc0ebe521589efdf021c1abf6ff3a26a30a688
Valtteri Huuskonen <valtteri.huuskonen@tuni.fi>
2023-04-03 23:55:42 +0300

    Add middleware to enforce authentication
    checks the JWT token and adds the userId from the JWT to the request

---
+++ backend/middleware/verifyAuth.js
@@ -0,0 +1,42 @@
+const { StatusCodes } = require("http-status-codes");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+const verifyAuth = (req, res, next) => {
+  if (req.method === "OPTIONS") {
+    return next();
+  }
+
+  try {
+    // Get token from Authorization header; format:
+    // Authorization: Bearer <token>
+    const token = req.headers?.authorization?.split(" ")[1] || ""; // handle missing authorization Header or token
+    if (!token) {
+      return res
+        .set("WWW-Authenticate", "Bearer ")
+        .status(StatusCodes.UNAUTHORIZED)
+        .send({ error: "Unauthorized" });
+    }
+
+    // Verify token
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+    // Add user data from the JWT to the request
+    req.userData = { userId: decodedToken.userId };
+    next();
+  } catch (err) {
+    // catch any errors (e.g. invalid token)
+    if (err.message === "jwt malformed") {
+      return res
+        .status(StatusCodes.UNAUTHORIZED)
+        .send({ error: "Invalid token" });
+    }
+    if (err.name === "TokenExpiredError") {
+      return res
+        .status(StatusCodes.UNAUTHORIZED)
+        .send({ error: "Token expired" });
+    }
+    return res.status(StatusCodes.UNAUTHORIZED).send({ error: "Unauthorized" });
+  }
+};
+
+module.exports = verifyAuth;
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const verifyAuth = require("../middleware/verifyAuth");
 
 const { signUpUser, loginUser } = require("../controllers/users");
 
 router.post("/signup", signUpUser);
 router.post("/login", loginUser);
 
+// Protect all routes below this middleware,
+// needs valid JWT in Authorization header
+router.use(verifyAuth);
 module.exports = router;
