c7ea2cfdcb5f9adbe5894aee10aa76343e44530b
Valtteri Huuskonen <valtteri.huuskonen@tuni.fi>
2023-04-04 01:14:15 +0300

    Add cascading deletions to DB tables
    - when user is deleted, delete also their listings
    - when listing is deleted, also delete the listing's pictures

--- db/init-scripts/dev/init.sql
+++ db/init-scripts/dev/init.sql
@@ -6,7 +6,9 @@ CREATE TABLE IF NOT EXISTS `users` (
     -- bcrypt hashes are 60 characters long
     password_hash VARCHAR(60) NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    -- delete user's listings when user is deleted:
+    CONSTRAINT user_listings_fk FOREIGN KEY (id) REFERENCES listings(owner) ON DELETE CASCADE
 );
 CREATE TABLE IF NOT EXISTS `listings` (
     listing_id BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
@@ -15,12 +17,14 @@ CREATE TABLE IF NOT EXISTS `listings` (
     asking_price DECIMAL(10, 2) NOT NULL,
     owner CHAR(36) CHARACTER SET ascii NOT NULL,
     location VARCHAR(255) NOT NULL,
-    FOREIGN KEY (owner) REFERENCES users(id)
+    FOREIGN KEY (owner) REFERENCES users(id),
+    -- delete listing's pictures when listing is deleted:
+    CONSTRAINT listing_pictures_fk FOREIGN KEY (listing_id) REFERENCES pictures(listing_id) ON DELETE CASCADE
 );
 CREATE TABLE IF NOT EXISTS `pictures` (
     id BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     url VARCHAR(1000) NOT NULL,
     blurhash VARCHAR(30) NOT NULL,
     listing_id BIGINT NOT NULL,
-    FOREIGN KEY (listing_id) REFERENCES listings(listing_id)
+    FOREIGN KEY (listing_id) REFERENCES listings(listing_id) ON DELETE CASCADE
 );
