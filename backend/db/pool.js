c36735b8300084060c66d4a1ae7b3b3519e1f865
Valtteri Huuskonen <valtteri.huuskonen@tuni.fi>
2023-04-03 12:27:33 +0300

    Add DB Port to the list of parameters that can be passed to the DB connection string

--- backend/db/pool.js
+++ backend/db/pool.js
@@ -7,6 +7,7 @@ const pool = mysql.createPool({
   user: process.env.MYSQL_USERNAME,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
+  port: process.env.MYSQL_PORT,
 });
 
 module.exports = pool;
