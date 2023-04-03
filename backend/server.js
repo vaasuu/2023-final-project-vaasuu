c060b1b78d48c56cd605b7a8cea50f5f5c8ae5c0
Valtteri Huuskonen <valtteri.huuskonen@tuni.fi>
2023-04-03 23:05:56 +0300

    Add check for required ENV vars at startup

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,23 @@ const app = require("./app");
 
 require("dotenv").config(); // loads .env file into process.env
 
+// Environment variables that are required for the app to run
+const requiredEnvVars = [
+  "JWT_SECRET",
+  "MYSQL_HOST",
+  "MYSQL_USERNAME",
+  "MYSQL_PASSWORD",
+  "MYSQL_DATABASE",
+  "MYSQL_PORT",
+];
+
+// Check that all required environment variables are set
+requiredEnvVars.forEach((envVar) => {
+  if (!process.env[envVar]) {
+    throw new Error(`Environment variable ${envVar} is required`);
+  }
+});
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
