cec3b98dadc59099f7e849108464736a15166729
Valtteri Huuskonen <valtteri.huuskonen@tuni.fi>
2023-04-12 21:42:30 +0300

    Add test for password reset email

---
+++ backend/__tests__/password-reset.test.js
@@ -0,0 +1,100 @@
+const {
+  describe,
+  expect,
+  beforeEach,
+  afterEach,
+  it,
+} = require("@jest/globals");
+const MailDev = require("maildev");
+const app = require("../app");
+const request = require("supertest");
+
+const waitMailDevShutdown = (maildev) => {
+  return new Promise((resolve) => {
+    maildev.close(() => resolve());
+  });
+};
+
+describe("Password reset", () => {
+  describe("password reset email", () => {
+    let maildev;
+
+    beforeEach((done) => {
+      maildev = new MailDev({
+        smtp: 1025,
+      });
+
+      maildev.listen(done);
+    });
+
+    afterEach(async () => {
+      await waitMailDevShutdown(maildev);
+      return new Promise((resolve) => {
+        maildev.removeAllListeners();
+        resolve();
+      });
+    });
+
+    it("should send an email for existing users", async () => {
+      const res = await request(app)
+        .post("/api/v1/password-reset/send-reset-email")
+        .send({
+          email: "jane.doe@example.com",
+        });
+
+      expect(res.statusCode).toEqual(200);
+      expect(res.body).toHaveProperty(
+        "message",
+        "Email sent if that email belongs to a user"
+      );
+
+      return new Promise((resolve, reject) => {
+        maildev.on("new", (email) => {
+          console.log(email);
+          try {
+            expect(email.subject).toEqual("Password Reset");
+            expect(email.to[0].address).toEqual("jane.doe@example.com"); // check receiver email address
+            expect(email.text).toMatch(/\/reset-password#ey/); // check for token link in email
+          } catch (error) {
+            reject(error);
+          }
+          resolve();
+        });
+      });
+    });
+
+    it("should not send an email if the user does not exist", async () => {
+      const res = await request(app)
+        .post("/api/v1/password-reset/send-reset-email")
+        .send({
+          email: "does.not.exist@test.example.com",
+        });
+
+      expect(res.statusCode).toEqual(200); // should be 200 as we don't want to give away that the email is not in the database
+      expect(res.body).toHaveProperty(
+        "message",
+        "Email sent if that email belongs to a user"
+      );
+
+      return new Promise((resolve, reject) => {
+        // wait for 1 second to see if an email is sent
+        setTimeout(() => {
+          resolve();
+        }, 1000);
+
+        maildev.on("new", (email) => {
+          console.log(email);
+          try {
+            expect(email.subject).not.toEqual("Password Reset");
+            expect(email.to[0].address).not.toEqual("jane.doe@example.com"); // check receiver email address
+            expect(email.text).not.toMatch(/\/reset-password#ey/); // check for token link in email
+          } catch (error) {
+            reject(error);
+          }
+          resolve();
+        });
+      });
+    });
+
+  });
+});
