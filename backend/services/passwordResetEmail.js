e310130c63a86224249bb5233d727c85c4cf1e6f
Valtteri Huuskonen <valtteri.huuskonen@tuni.fi>
2023-04-13 01:29:51 +0300

    Add notice of password reset link expiration to the email

--- backend/services/passwordResetEmail.js
+++ backend/services/passwordResetEmail.js
@@ -23,10 +23,12 @@ const sendPasswordResetEmail = async (user, resetToken) => {
     text: `You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n
         Please click on the following link, or paste it into your browser to complete the process:\n\n
         ${resetUrl}\n\n
+        This link will expire in 30 minutes.\n\n
         If you did not request this, please ignore this email and your password will remain unchanged.\n`,
     html: `<p>You are receiving this because you (or someone else) have requested the reset of the password for your account.</p>
         <p>Please click on the following link, or paste it into your browser to complete the process:</p>
         <a href="${resetUrl}">${resetUrl}</a>
+        <p>This link will expire in 30 minutes.</p>
         <p>If you did not request this, please ignore this email and your password will remain unchanged.</p>`,
   };
 
