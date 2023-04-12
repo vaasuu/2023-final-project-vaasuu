1e9155090f6cdedf219785aac224c5dab5099a54
Valtteri Huuskonen <valtteri.huuskonen@tuni.fi>
2023-04-13 00:07:23 +0300

    Add tests for password reset: set new password endpoint

--- backend/__tests__/password-reset.test.js
+++ backend/__tests__/password-reset.test.js
@@ -5,10 +5,13 @@ const {
   afterEach,
   it,
   afterAll,
+  beforeAll,
 } = require("@jest/globals");
 const MailDev = require("maildev");
 const app = require("../app");
 const request = require("supertest");
+const jwt = require("jsonwebtoken");
+const users = require("../models/users");
 
 const waitMailDevShutdown = (maildev) => {
   return new Promise((resolve) => {
@@ -99,5 +102,98 @@ describe("Password reset", () => {
       });
     });
 
+    describe("POST set-new-password", () => {
+      let user;
+      let token;
+
+      beforeAll(async () => {
+        user = {
+          id: "bbbbbbbb-f9e0-4047-99a5-6f0ed153ba89", // Jane Doe
+        };
+
+        token = jwt.sign(
+          { id: user.id },
+          process.env.JWT_PASSWORD_RESET_SECRET,
+          { expiresIn: "30m" } // token expires in 30 minutes
+        );
+      });
+
+      it("should let you set a new password with a valid password reset token", async () => {
+        const passwordHashBefore = await users
+          .findById(user.id)
+          .then((users) => users[0].password_hash);
+
+        const res = await request(app)
+          .post("/api/v1/password-reset/set-new-password")
+          .send({
+            token: token,
+            password: "jane.doe", // new password (same as old password for testing)
+          });
+
+        expect(res.statusCode).toEqual(204);
+        expect(res.body).toEqual({});
+
+        const passwordHashAfter = await users
+          .findById(user.id)
+          .then((users) => users[0].password_hash);
+
+        expect(passwordHashBefore).not.toEqual(passwordHashAfter);
+      });
+
+      it("should not let you set a new password with an invalid password reset token", async () => {
+        const res = await request(app)
+          .post("/api/v1/password-reset/set-new-password")
+          .send({
+            token: "invalid-token",
+            password: "jane.doe", // new password (same as old password for testing)
+          });
+
+        expect(res.statusCode).toEqual(401);
+        expect(res.body).toHaveProperty("error", "Unauthorized");
+      });
+
+      it("should not let you set a new password with an expired password reset token", async () => {
+        const res = await request(app)
+          .post("/api/v1/password-reset/set-new-password")
+          .send({
+            token:
+              "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImJiYmJiYmJiLWY5ZTAtNDA0Ny05OWE1LTZmMGVkMTUzYmE4OSIsImlhdCI6MTY4MTMyOTU5MiwiZXhwIjoxNjgxMzMxMzkyfQ.AmYqIHxjChUcZY2eSSgrqjW8U88D2fMP2MkVH6csoyQ", // expired token
+            password: "jane.doe", // new password (same as old password for testing)
+          });
+
+        expect(res.statusCode).toEqual(401);
+        expect(res.body).toHaveProperty("error", "Token expired");
+      });
+
+      it("should not let you set a new password with an empty password", async () => {
+        const res = await request(app)
+          .post("/api/v1/password-reset/set-new-password")
+          .send({
+            token: token,
+            password: "",
+          });
+
+        expect(res.statusCode).toEqual(400);
+        expect(res.body).toHaveProperty(
+          "error",
+          '"password" is not allowed to be empty'
+        );
+      });
+
+      it("should not let you set a new password with a password that is too short", async () => {
+        const res = await request(app)
+          .post("/api/v1/password-reset/set-new-password")
+          .send({
+            token: token,
+            password: "short", // minimum password length is 8 characters
+          });
+
+        expect(res.statusCode).toEqual(400);
+        expect(res.body).toHaveProperty(
+          "error",
+          '"password" length must be at least 8 characters long'
+        );
+      });
+    });
   });
 });
