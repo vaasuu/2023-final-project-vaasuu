cec519942360f3a8f61e7192e0a44b6db2e69927
Valtteri Huuskonen <valtteri.huuskonen@tuni.fi>
2023-04-17 15:01:55 +0300

    Switch from https library to node-fetch
    It seems easier to use than `https` even though node 18 has fetch.
    Buffer is easier with node-fetch.

--- backend/utils/blurhashHelper.js
+++ backend/utils/blurhashHelper.js
@@ -1,33 +1,19 @@
-const https = require("https");
+const fetch = require("node-fetch");
 const sharp = require("sharp");
 const { encode } = require("blurhash");
 const logger = require("./log");
 
-const downloadImage = (url) =>
-  new Promise((resolve, reject) => {
-    https.get(url, (response) => {
-      if (response.statusCode !== 200) {
-        logger.error(
-          `Image download failed! status: ${response.statusCode} url: ${url}`
-        );
-        return reject(
-          new Error(`Problem downloading image from URL ( ${url} )`)
-        );
-      }
-      const data = [];
-      response
-        .on("data", (chunk) => {
-          data.push(chunk);
-        })
-        .on("end", () => {
-          const buffer = Buffer.concat(data);
-          resolve(buffer);
-        })
-        .on("error", (error) => {
-          reject(error);
-        });
-    });
-  });
+const downloadImage = async (url) => {
+  const response = await fetch(url);
+
+  if (!response.ok) {
+    logger.error(`HTTP error! status: ${response.status}`);
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+
+  const buffer = await response.buffer();
+  return buffer;
+};
 
 const encodeImageToBlurhash = async (image) =>
   new Promise((resolve, reject) => {
