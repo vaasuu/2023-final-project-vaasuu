fc0911d6bd014d1f1e091a61609b877ba6d02019
Valtteri Huuskonen <valtteri.huuskonen@tuni.fi>
2023-04-24 04:16:21 +0300

    Add terraform config to generate AWS RDS instance

---
+++ terraform/variables.tf
@@ -0,0 +1,18 @@
+variable "aws_access_key_id" {
+  description = "Access key to AWS console"
+}
+variable "aws_secret_access_key" {
+  description = "Secret key to AWS console"
+}
+variable "aws_session_token" {
+  description = "Temporary session token to AWS console"
+}
+variable "region" {
+  description = "AWS region"
+}
+variable "db_username" {
+  description = "Database username"
+}
+variable "db_password" {
+  description = "Database password"
+}
