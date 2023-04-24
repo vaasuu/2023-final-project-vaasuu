d6b600337c8fd6256e44eacc0d125f4948c1d8b6
Valtteri Huuskonen <valtteri.huuskonen@tuni.fi>
2023-04-24 21:03:26 +0300

    Add better 404 page

--- frontend/src/components/pages/NotFound/NotFound.jsx
+++ frontend/src/components/pages/NotFound/NotFound.jsx
@@ -1,7 +1,15 @@
+import notFoundIllustration from "../../../assets/notFoundPage/undraw_not_found_re_bh2e.svg";
+
+import "./NotFound.css";
+
 const NotFound = () => {
+  document.title = "Page Not Found | Marketplace";
+
   return (
-    <div>
-      <h1>NotFound</h1>
+    <div className="container">
+      <h1>Unfortunatly the page you are looking for does not exist.</h1>
+
+      <img className="notFound-illustration" src={notFoundIllustration} />
     </div>
   );
 };
