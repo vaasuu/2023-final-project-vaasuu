56d92967667c1a2b1d3f56785185dfd520e2cb06
Valtteri Huuskonen <valtteri.huuskonen@tuni.fi>
2023-04-25 04:26:15 +0300

    Add auth form

--- frontend/src/components/pages/Auth/Auth.jsx
+++ frontend/src/components/pages/Auth/Auth.jsx
@@ -1,9 +1,105 @@
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+
+import "./Auth.css";
+
 const Auth = () => {
-  document.title = "Auth | Marketplace";
+  const [isLogin, setIsLogin] = useState(true);
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+
+  document.title = isLogin ? "Login | Marketplace" : "Register | Marketplace";
 
   return (
-    <div>
-      <h1>Auth</h1>
+    <div className="container auth-form">
+      <h1>{isLogin ? "Log in" : "Register an account"}</h1>
+      <button onClick={() => setIsLogin(!isLogin)}>
+        {isLogin ? "Register" : "Login"} instead?
+      </button>
+
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="form-control">
+          {!isLogin && (
+            <>
+              <label htmlFor="name">Name</label>
+              <input
+                type="text"
+                id="name"
+                placeholder="John Doe"
+                {...register("name", {
+                  required: true,
+                  maxLength: 255,
+                })}
+              />
+              {errors.name && (
+                <p role="alert" className="form-error">
+                  Name is required
+                </p>
+              )}
+            </>
+          )}
+
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            placeholder="example@example.com"
+            {...register("email", { required: true, pattern: /^\S+@\S+$/i })}
+          />
+
+          {errors.email && (
+            <p role="alert" className="form-error">
+              Email is required
+            </p>
+          )}
+
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            minLength={8}
+            maxLength={72}
+            placeholder="********"
+            {...register("password", {
+              required: true,
+              minLength: 8,
+              maxLength: 72,
+            })}
+          />
+
+          {errors.password && (
+            <p className="form-error">Password is required</p>
+          )}
+
+          {!isLogin && (
+            <>
+              <label htmlFor="confirmPassword">Confirm Password</label>
+              <input
+                type="password"
+                id="confirmPassword"
+                minLength={8}
+                maxLength={72}
+                placeholder="********"
+                {...register("confirmPassword", {
+                  required: true,
+                  validate: (value) => value === watch("password"),
+                  minLength: 8,
+                  maxLength: 72,
+                })}
+              />
+              {errors.confirmPassword && (
+                <p className="form-error">Passwords do not match</p>
+              )}
+            </>
+          )}
+          <input type="submit" />
+        </div>
+      </form>
     </div>
   );
 };
