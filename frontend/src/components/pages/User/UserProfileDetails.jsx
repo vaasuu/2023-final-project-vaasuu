f9a959d6ec8f010116c82f1536b5619cbda446de
Valtteri Huuskonen <valtteri.huuskonen@tuni.fi>
2023-04-29 03:30:21 +0300

    Add edit profile button to user page

--- frontend/src/components/pages/User/UserProfileDetails.jsx
+++ frontend/src/components/pages/User/UserProfileDetails.jsx
@@ -1,6 +1,13 @@
+import { Link } from "react-router-dom";
 import "./UserProfileDetails.css";
+import { GrSettingsOption } from "react-icons/gr";
+import { AuthContext } from "../../../shared/context/auth-context";
+import { useContext } from "react";
 
 const UserProfileDetails = ({ user }) => {
+  const auth = useContext(AuthContext);
+  const shouldShowEditProfileButton = auth.userId === user.id || auth.isAdmin;
+
   return (
     <div className="user-profile-details">
       <div className="user-profile-details__avatar">
@@ -10,6 +17,13 @@ const UserProfileDetails = ({ user }) => {
       <div className="user-profile-details__joined-at">
         Joined on: {new Date(user.created_at).toLocaleDateString()}
       </div>
+      {shouldShowEditProfileButton && (
+        <div className="user-profile-details__edit-profile-button">
+          <Link id="editProfileLink" to={`/market/users/${user.id}/edit`}>
+            <GrSettingsOption />
+          </Link>
+        </div>
+      )}
     </div>
   );
 };
