07c5c1c9afd0dd0858d66ada05fab4148436e745
Valtteri Huuskonen <valtteri.huuskonen@tuni.fi>
2023-04-30 04:26:16 +0300

    Add listings & listings search

--- frontend/src/api/listings/listings.js
+++ frontend/src/api/listings/listings.js
@@ -23,3 +23,54 @@ export const getUserListings = async (userId, token) => {
     }
   });
 };
+
+export const getAllListings = async (token) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const res = await fetch(`${BACKEND_URL}/api/v1/listings`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      if (res.status === 200) {
+        const data = await res.json();
+        resolve(data);
+      } else {
+        const data = await res.json();
+        reject(data);
+      }
+    } catch (err) {
+      reject(err);
+    }
+  });
+};
+
+export const searchListings = async (token, searchTerm) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const res = await fetch(
+        `${BACKEND_URL}/api/v1/listings/search?query=${searchTerm}`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      if (res.status === 200) {
+        const data = await res.json();
+        resolve(data);
+      } else {
+        const data = await res.json();
+        reject(data);
+      }
+    } catch (err) {
+      reject(err);
+    }
+  });
+};
