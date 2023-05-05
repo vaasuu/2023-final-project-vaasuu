29f8f63e08bd620b62ab2938dc830dde5f62c8ff
Valtteri Huuskonen <valtteri.huuskonen@tuni.fi>
2023-05-05 11:44:04 +0300

    Add login command

--- frontend/cypress/support/commands.js
+++ frontend/cypress/support/commands.js
@@ -23,3 +23,10 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add("login", (email, password) => {
+  cy.visit("http://localhost:5173/auth");
+  cy.get("input[name=email]").type(email);
+  cy.get("input[name=password]").type(password);
+  cy.get('[type="submit"]').click();
+});
