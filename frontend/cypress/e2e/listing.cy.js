74b8c109a410771ed42ea5ae511840d2de5fd739
Valtteri Huuskonen <valtteri.huuskonen@tuni.fi>
2023-05-05 20:46:42 +0300

    Add listing e2e tests

---
+++ frontend/cypress/e2e/listing.cy.js
@@ -0,0 +1,32 @@
+describe("listing", () => {
+  it("loads", () => {
+    cy.login("john.smith@example.com", "john.smith");
+    cy.visit("http://localhost:5173/market/listings/1");
+  });
+  it("can be edited by owner", () => {
+    cy.createListing(); // create a listing to edit
+    cy.contains("Edit").click();
+    cy.url().should("include", "/edit");
+    cy.contains("Edit Listing");
+    cy.get("#title").should("have.value", "test title");
+    cy.get("#category").should("have.value", "electronics");
+    cy.get("#price").should("have.value", "1000.00");
+
+    // Edit listing details
+    cy.get("#price").clear().type("2000.00");
+    cy.get('[type="submit"]').click();
+    cy.contains("test title");
+    cy.contains("2000.00");
+  });
+
+  it("can be deleted by owner", () => {
+    cy.createListing("test title deletion"); // create a listing to delete
+    cy.contains("Edit").click();
+    cy.url().should("include", "/edit");
+    cy.contains("Edit Listing");
+    cy.contains("Delete Listing").click();
+    cy.url().should("match", /\/listings$/);
+    cy.contains("No listings found").should("not.exist");
+    cy.contains("test title deletion").should("not.exist");
+  });
+});
--- frontend/cypress/support/commands.js
+++ frontend/cypress/support/commands.js
@@ -38,3 +38,30 @@ Cypress.Commands.add("openListingsPage", () => {
   cy.contains("Listings");
   cy.url().should("include", "/listings");
 });
+
+Cypress.Commands.add("createListing", (title, description) => {
+  cy.login("john.smith@example.com", "john.smith");
+  cy.visit("http://localhost:5173/market/listings/new");
+  cy.get("#title").type(title || "test title");
+  cy.get("#description").type(description || "test description");
+  cy.get("#category").select("electronics");
+  cy.get("#price").type("1000.00");
+  cy.get("#currency").select("USD");
+  cy.get("#location").type("test location");
+  cy.get("#image_urls").type("https://placekitten.com/200/300");
+
+  cy.contains("Image URLs").click(); // click out of image url input so onBlur event is triggered
+
+  cy.get(".listing-form__image-urls-list > li > a").should(
+    "have.attr",
+    "href",
+    "https://placekitten.com/200/300"
+  ); // check that image was actually added
+
+  cy.get('[type="submit"]').click();
+  cy.url().should("match", /\/listings\/\d+$/);
+  cy.contains("test title");
+  cy.contains("test description");
+  cy.contains("1000.00");
+  cy.contains("test location");
+});
